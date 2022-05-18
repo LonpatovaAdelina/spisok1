1084044717bd1f2b5ab7ca2791257932024112fe
LonpatovaAdelina <91686774+LonpatovaAdelina@users.noreply.github.com>
2022-05-18 13:32:29 +0300

    Create list1_func.h

---
+++ list1_func.h
@@ -0,0 +1,12 @@
+#pragma once
+struct elem
+{
+	int a;
+	elem* next = nullptr;
+};
+void add(elem*& list, int data);
+void insert(elem*& list, int data, int position);
+const int* get(const elem* list, int position);
+int count(const elem* list);
+bool remove(elem*& list, int position);
+void clear(elem*& list);
